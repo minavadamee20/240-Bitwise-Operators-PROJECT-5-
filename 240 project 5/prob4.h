bf506f8a82ae1a4a2f7453a17c42ab3a8c63aa98
minavadamee20 <34077203+minavadamee20@users.noreply.github.com>
2018-10-02 21:01:40 -0700

    commit default

--- 240 project 5/prob4.h
+++ 240 project 5/prob4.h
@@ -5,31 +5,19 @@
 /*The sSheaton Hotel is a 16th floor building. Suppose each bits of AX register represent one floor (the right most bit is representing the first floor). Ten people got into the elevator on the first floor 
 and each pressed a random floor number between 2 and 16 (some random numbers may be the same floor). Write a program to (i)) display the content of AX at base 2 (ii) use AX register to determine at which floors the elevator will stop.*/
 
-int a[4];
+int a[16];
 
 void assembly() {
 	_asm {
-		lea esi, a;
-		mov[esi], 10;
-		add esi, 4;
-		mov[esi], 15;
-		add esi, 4;
-		mov[esi], 30;
+		mov ax, 1001000100001100b;
+		lea esi, a; 
+		mov bx, 1b;
+
+		
+
 	}
-	for (int counter = 0; counter < 4; counter++) {
-		std::cout << "[ " << a[counter] << " ] --> ";
-	}
-	std::cout << std::endl;
+	std::cout << a;
 }
-
-
-
-
-
-
-
-
-
 
 
 
